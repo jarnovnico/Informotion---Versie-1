a8fc4444bed3acd0cf53bff0e85757f754b4be3d
jaimiederijk <j.r.derijk@quicknet.nl>
2015-11-05 14:29:00 +0100

    props based on data

--- src/assets/scripts/playcanvas/addgarbage.js
+++ src/assets/scripts/playcanvas/addgarbage.js
@@ -9,85 +9,90 @@ pc.script.create('addgarbage', function (app) {
         initialize: function () {
             this.addGarbage ();
         },
+
         addGarbage: function () {
-            var dataGarbage = [5,3,4,2,1,4,4,3,3,1];
-            
+
+            function processData (gardata,entity) {
+                var totalGar = 0;
+                var averageGar;
+                for (var i = gardata.subparts.length - 1; i >= 0; i--) {
+                    totalGar += gardata.subparts[i].grade;
+                }
+                averageGar= Math.round(totalGar/gardata.subparts.length);
+                selectProp(averageGar,entity);
+            };
+            var dataGarbage = graphData.schoonheidsgraden;
+
             function selectProp (data,entity) {
                 var x =0.4;
-                if (data==1) {
-                    if (entity.name == "vuilnis-schaal1") {
-                        entity.setLocalScale(x,x,x);
-                    }                
-                } else if (data==2) {
-                    if (entity.name == "vuilnis-schaal2") {
-                        entity.setLocalScale(x,x,x);
-                    }  
-                } else if (data==3) {
-                    if (entity.name == "vuilnis-schaal3") {
-                        entity.setLocalScale(x,x,x);
-                    }  
-                } else if (data==4) {
-                    if (entity.name == "vuilnis-schaal4") {
-                        entity.setLocalScale(x,x,x);
-                    }  
-                } else if (data==5) {
+                if (data==1||data==2) {
                     if (entity.name == "vuilnis-schaal5") {
                         entity.setLocalScale(x,x,x);
-                    }  
+                    }
+                } else if (data==3||data==4) {
+                    if (entity.name == "vuilnis-schaal4") {
+                        entity.setLocalScale(x,x,x);
+                    }
+                } else if (data==5||data==6) {
+                    if (entity.name == "vuilnis-schaal3") {
+                        entity.setLocalScale(x,x,x);
+                    }
+                } else if (data==7||data==8) {
+                    if (entity.name == "vuilnis-schaal2") {
+                        entity.setLocalScale(x,x,x);
+                    }
+                } else if (data==9||data==10) {
+                    if (entity.name == "vuilnis-schaal1") {
+                        entity.setLocalScale(x,x,x);
+                    }
                 }
             }
             this.entity.setLocalScale(0.01,0.01,0.01);
             //weteringenschans
             if(this.entity._parent.name==globals.cityParts[0].name) {
                 this.entity.setLocalPosition(-0.79,3.6,3.54);
-                selectProp(dataGarbage[0], this.entity);
+                processData(dataGarbage[0], this.entity);
                 //globals.cityParts[0].findByName("gebouwen-euro2").setLocalScale(0.01,0.01,0.01);
             }
             //westerlijke-eilanden
             else if(this.entity._parent.name==globals.cityParts[1].name) {
                 this.entity.setLocalPosition(-0.79,3.6,-2.9);
-                selectProp(dataGarbage[1], this.entity);
+                processData(dataGarbage[1], this.entity);
+            }//jordaan
+            else if(this.entity._parent.name==globals.cityParts[5].name) {
+                this.entity.setLocalPosition(-3.09,3.6,0.8);
+                processData(dataGarbage[5], this.entity);
+            }//grachtengordel-west
+            else if(this.entity._parent.name==globals.cityParts[7].name) {
+                this.entity.setLocalPosition(-1.86,3.6,-0.564);
+                processData(dataGarbage[7], this.entity);
+            }//grachtengordel-zuid
+            else if(this.entity._parent.name==globals.cityParts[6].name) {
+                this.entity.setLocalPosition(0.458,3.6,2.8);
+                processData(dataGarbage[6], this.entity);
             }
             //plantage
             else if(this.entity._parent.name==globals.cityParts[2].name) {
                 this.entity.setLocalPosition(2.08,3.6,2.7);
-                selectProp(dataGarbage[2], this.entity);
+                processData(dataGarbage[2], this.entity);
+            }//burgwallen-nieuwe-zijde
+            else if(this.entity._parent.name==globals.cityParts[9].name) {
+               processData(dataGarbage[9], this.entity);
+                this.entity.setLocalPosition(0.6,3.6,-1.6);
+            }//kadijken
+            else if(this.entity._parent.name==globals.cityParts[4].name) {
+                this.entity.setLocalPosition(4.38,3.6,0.8);
+                processData(dataGarbage[4], this.entity);
             }
             //nieuwmarkt
             else if(this.entity._parent.name==globals.cityParts[3].name) {
                 this.entity.setLocalPosition(1.78,3.6,0.76);
-                selectProp(dataGarbage[3], this.entity);
-            }
-            //kadijken
-            else if(this.entity._parent.name==globals.cityParts[4].name) {
-                this.entity.setLocalPosition(4.38,3.6,0.8);
-                selectProp(dataGarbage[4], this.entity);
-            }
-            //jordaan
-            else if(this.entity._parent.name==globals.cityParts[5].name) {
-                this.entity.setLocalPosition(-3.09,3.6,0.8);
-                selectProp(dataGarbage[5], this.entity);
-            }
-            //grachtengordel-zuid
-            else if(this.entity._parent.name==globals.cityParts[6].name) {
-                this.entity.setLocalPosition(0.458,3.6,2.8);
-                selectProp(dataGarbage[6], this.entity);
-            }
-            //grachtengordel-west
-            else if(this.entity._parent.name==globals.cityParts[7].name) {
-                this.entity.setLocalPosition(-1.86,3.6,-0.564);
-                selectProp(dataGarbage[7], this.entity);
+                processData(dataGarbage[3], this.entity);
             }
             //burgwallen-oost-zijde
             else if(this.entity._parent.name==globals.cityParts[8].name) {
-                
-                selectProp(dataGarbage[8], this.entity);
+                processData(dataGarbage[8], this.entity);
                 this.entity.setLocalPosition(-0.2,3.6,0.96);
-            }
-            //burgwallen-nieuwe-zijde
-            else if(this.entity._parent.name==globals.cityParts[9].name) {
-               selectProp(dataGarbage[9], this.entity);
-                this.entity.setLocalPosition(0.6,3.6,-1.6);
             }
         },
 
--- src/assets/scripts/playcanvas/addprops.js
+++ src/assets/scripts/playcanvas/addprops.js
@@ -11,28 +11,35 @@ pc.script.create('addprops', function (app) {
         initialize: function () {
             this.addProps();
         },
-        
+
+
         addProps : function () {
-            var dataMoney = [5,3,4,2,1,4,4,3,3,1];
-            
+            var dataMoney = graphData.hotelData
+
+            function processData (hoteldata,entity) {
+                var averageHotel;
+
+                averageHotel= Math.round(hoteldata[1].prijs);
+                selectProp(averageHotel,entity);
+            };
             function selectProp (data,entity) {
-                if (data==1) {
+                if (data<100) {
                     if (entity.name == "gebouwen-euro1") {
                         entity.setLocalScale(0.2,0.2,0.2);
-                    }                
-                } else if (data==2) {
+                    }
+                } else if (data>99&&data<150) {
                     if (entity.name == "gebouwen-euro2") {
                         entity.setLocalScale(0.2,0.2,0.2);
                     }  
-                } else if (data==3) {
+                } else if (data>149&&data<200) {
                     if (entity.name == "gebouwen-euro3") {
                         entity.setLocalScale(0.2,0.2,0.2);
                     }  
-                } else if (data==4) {
+                } else if (data>199&&data<250) {
                     if (entity.name == "gebouwen-euro4") {
                         entity.setLocalScale(0.2,0.2,0.2);
                     }  
-                } else if (data==5) {
+                } else if (data>249&&data<350) {
                     if (entity.name == "gebouwen-euro5") {
                         entity.setLocalScale(0.2,0.2,0.2);
                     }  
@@ -47,53 +54,53 @@ pc.script.create('addprops', function (app) {
             //weteringenschans
             if(this.entity._parent.name==globals.cityParts[0].name) {
                 this.entity.setLocalPosition(-1.79,3.6,2.7);
-                selectProp(dataMoney[0], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
                 //globals.cityParts[0].findByName("gebouwen-euro2").setLocalScale(0.01,0.01,0.01);
             }
             //westerlijke-eilanden
             else if(this.entity._parent.name==globals.cityParts[1].name) {
                 this.entity.setLocalPosition(-1.85,3.6,-3.31);
-                selectProp(dataMoney[1], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
             }
             //plantage
             else if(this.entity._parent.name==globals.cityParts[2].name) {
                 this.entity.setLocalPosition(3.5,3.6,2.2);
-                selectProp(dataMoney[2], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
             }
             //nieuwmarkt
             else if(this.entity._parent.name==globals.cityParts[3].name) {
                 this.entity.setLocalPosition(2.2,3.6,-1.3);
-                selectProp(dataMoney[3], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
             }
             //kadijken
             else if(this.entity._parent.name==globals.cityParts[4].name) {
                 this.entity.setLocalPosition(5.6,3.6,0.9);
-                selectProp(dataMoney[4], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
             }
             //jordaan
             else if(this.entity._parent.name==globals.cityParts[5].name) {
                 this.entity.setLocalPosition(-2.9,3.6,-0.29);
-                selectProp(dataMoney[5], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
             }
             //grachtengordel-zuid
             else if(this.entity._parent.name==globals.cityParts[6].name) {
                 this.entity.setLocalPosition(-0.87,3.6,2.06);
-                selectProp(dataMoney[6], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
             }
             //grachtengordel-west
             else if(this.entity._parent.name==globals.cityParts[7].name) {
                 this.entity.setLocalPosition(-1.7,3.6,0.737);
-                selectProp(dataMoney[7], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
             }
             //burgwallen-oost-zijde
             else if(this.entity._parent.name==globals.cityParts[8].name) {
                 
-                selectProp(dataMoney[8], this.entity);
+                processData(dataMoney[this.entity._parent.name], this.entity);
                 this.entity.setLocalPosition(0.348,3.6,-0.194);
             }
             //burgwallen-nieuwe-zijde
             else if(this.entity._parent.name==globals.cityParts[9].name) {
-               selectProp(dataMoney[9], this.entity);
+               processData(dataMoney[this.entity._parent.name], this.entity);
                 this.entity.setLocalPosition(-0.45,3.6,-0.8);
             }            
         },
